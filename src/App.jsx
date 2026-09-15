d5ec4b126a75a592c9be0c122067f1dcd159318c
Raphael <raphaelmasc@gmail.com>
2026-09-15 11:04:47 -0300

    chore(style):Instalando e configurando o tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-houder-dom"
+import NavBar from "./components/Navbar"
+import Home from "./pages/Home"
+import Sobre from "./pages/Sobre"
+import Produtos from "./pages/Produtos"
+import Contato from "./pages/Contato"
 const App = () => {
   return (
-    <>
-      
-    </>
+    <Router>
+
+    </Router>
   )
 }
 
