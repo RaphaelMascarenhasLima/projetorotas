228bd0f19b8f4fa4fdc3ae6ed747b6023a829095
Raphael <raphaelmasc@gmail.com>
2026-09-15 11:48:41 -0300

    style: Estilizando o navbar

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Pacote de dependências de rotas.
 npm install react-router-dom
+# Pacote de dependências de icones
+npm install lucide-react
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from "react-houder-dom"
-import NavBar from "./components/Navbar"
-import Home from "./pages/Home"
-import Sobre from "./pages/Sobre"
-import Produtos from "./pages/Produtos"
-import Contato from "./pages/Contato"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Home from './pages/Home'
+import Sobre from './pages/Sobre'
+import Produtos from './pages/Produtos'
+import Contato from './pages/Contato'
+
 const App = () => {
   return (
     <Router>
+      <div className='min-h-screen bg-gray-200 text-gray-900'>
+        <NavBar/>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sobre" element={<Sobre />} />
+            <Route path="/produtos" element={<Produtos />} />
+            <Route path="/contato" element={<Contato />} />
+          </Routes>
+        </main>
 
+      </div>
     </Router>
   )
 }
